fff8a981ef9ec6584911ec29e66bd3f0a2303963
Rafael Freitas <oraffaudi@gmail.com>
2019-05-10 07:47:58 -0300

    feature: add Watson natural language understanding service

--- credentials/README.md
+++ credentials/README.md
@@ -10,3 +10,20 @@ File: `algorithmia.json`
 }
 
 ```
+
+
+
+## Watson Natural Language Understading
+
+File: `watson-nlu.json`
+
+```
+{
+  "apikey": "sYLcPUxWF5hoCjUprBXMkXjBCr8oxYRpDM6loWB1TgAl",
+  "iam_apikey_description": "Auto-generated for key 38eb82df-2b38-4797-833c-2aa05ff4e7a7",
+  "iam_apikey_name": "Auto-generated service credentials",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/394394534b6e73a21247217749a96149::serviceid:ServiceId-28bcf00e-5b4b-48cf-b75e-8973f873ed8c",
+  "url": "https://gateway.watsonplatform.net/natural-language-understanding/api"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -1,11 +1,26 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+
+const NaturalLanguageUnderstadingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+
+
+var nlu = new NaturalLanguageUnderstadingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
+
+
 
 async function robot(content) {
     await fetchContentFromWikipedia(content)
-    sanititeContent(content)
+    sanitizeContent(content)
     breakContentIntoSetences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -16,7 +31,7 @@ async function robot(content) {
         content.sourceContentOriginal = wikipideaContent.content
     }
 
-    function sanititeContent(content){
+    function sanitizeContent(content){
         const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
         const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
 
@@ -51,9 +66,36 @@ async function robot(content) {
                 images: []
             })
         })
-        console.log(sentences)
     }
 
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content){
+        for (const sentence of content.sentences){
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence){
+        return new Promise((resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            }, (error, response) => {
+                if (error){
+                    throw error
+                }
+    
+                const keywords = response.keywords.map((keyword) => { return keyword.text})
+    
+                resolve(keywords)
+            })
+        })
+    }
 }
 
 
