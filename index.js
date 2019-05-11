5fead423bfbbcff5b82a722a9865014efee9c8c2
Rafael Freitas <oraffaudi@gmail.com>
2019-05-10 21:28:13 -0300

    feature: add google images search

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const robots = {
   input: require('./robots/input.js'),
   text: require('./robots/text.js'),
-  state: require('./robots/state.js')
+  state: require('./robots/state.js'),
+  image: require('./robots/imagem.js')
 }
 
 
-async function start() {
+async function start() { 
 
   robots.input()
   await robots.text()
+  await robots.image()
 
   const content = robots.state.load()
   console.dir(content,{ depth: null})
